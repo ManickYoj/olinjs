3bcbd20b5d135c495559fc59f3e4f75a45fe68f5
manickyoj <hotshot354@gmail.com>
2015-02-10 22:21:44 -0500

    Header format chosen.

--- homeworks/class6/index.js
+++ homeworks/class6/index.js
@@ -15,7 +15,11 @@ var PORT = process.env.PORT || 3000;
 var mongoURI = process.env.MONGOURI || "mongodb://localhost/test";
 mongoose.connect(mongoURI);
 
-app.engine('hbs', require('express-handlebars')());
+var hbsOptions = {
+	layoutsDir: __dirname + '/views/layouts',
+	defaultLayout: __dirname + '/views/layouts/main.hbs'
+};
+app.engine('hbs', require('express-handlebars')(hbsOptions));
 app.set('views', path.join(__dirname, 'views', 'pages'));
 app.set('view engine', 'hbs');
 
