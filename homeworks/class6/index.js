bebf89af356b6e66ef7897da5b8c5bf160474a59
manickyoj <hotshot354@gmail.com>
2015-02-10 19:30:42 -0500

    Twotter boilerplate

---
+++ homeworks/class6/index.js
@@ -0,0 +1,33 @@
+// Utility Imports
+var express = require('express');
+var mongoose = require('mongoose');
+var path = require('path');
+var logger = require('morgan');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+
+// Startup
+var app = express();
+
+// Config
+var PORT = process.env.PORT || 3000;
+
+var mongoURI = process.env.MONGOURI || "mongodb://localhost/test";
+mongoose.connect(mongoURI);
+
+app.engine('hbs', require('express-handlebars')());
+app.set('views', path.join(__dirname, 'views', 'pages'));
+app.set('view engine', 'hbs');
+
+// Middleware
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, "public")));
+
+// Routing Table
+app.get('/', function(req, res) { res.render('index'); });
+
+// Listen
+app.listen(PORT);
